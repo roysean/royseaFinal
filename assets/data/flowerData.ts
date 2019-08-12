09c15747244e9499076f7859d80aef9734caecd0
roysean <52671269+roysean@users.noreply.github.com>
2019-08-12 12:31:16 -0400

    Add files via upload

---
+++ assets/data/flowerData.ts
@@ -0,0 +1,7 @@
+import { Flower } from '../../app/flower';
+
+export const FLOWERDATA: Flower[] = [
+    {name: "Lily", colour: "Yellow", country: "Canada" },
+    {name: "Rose", colour: "Red", country: "Switzerland" },
+    {name: "Lavendar", colour: "Lavendar", country: "Japan" }
+];
